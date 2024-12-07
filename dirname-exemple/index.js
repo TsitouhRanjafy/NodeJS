b205c779d0ee7d36a8468bf15a581134fd5b2b1c
Jackweb-rgba <radohery09@gmail.com>
2024-12-07 19:02:15 +0100

    Difference between process.cwd() vs _dirname

--- dirname-exemple/index.js
+++ dirname-exemple/index.js
@@ -7,22 +7,22 @@ const dirPah = path.join(__dirname,'/pictures');
 const filePath = path.join(__dirname,'/pictures','text.txt');
 
 // create a new directory '/pictures'
-/* fs.mkdirSync(dirPah) */
+fs.mkdirSync(dirPah) 
 
 // open file as blob
-/* (async () => {
+(async () => {
     const blob = await openAsBlob(path.join(__dirname,'/pictures','text.txt'),{ type: "soratra" });
     const ab = await blob.arrayBuffer();
     // blob.stream();
     console.log(blob);
     console.log(ab);
     
-})();*/
+})();
 
 express.static(path.join(__dirname,'/public'));
 
 // Open (created or overwritten) and write something in a file
-/*const fd = fs.openSync(filePath,'w',11)
+const fd = fs.openSync(filePath,'w',11)
 
 fs.writeSync(fd,'ito soratra hipetaka ao');
 
@@ -33,7 +33,32 @@ setTimeout(() => {
 },10000)
 
 console.log(fd);
-console.log('Program done!'); */
+console.log('Program done!'); 
+
+// Difference between process.cwd() vs __dirname
+/**
+ * Project 
+ *  |___ main.js
+ *  |___ lib
+ *      |__ script.js
+ * 
+ * process.cwd() returns the value of directory where we run the node process.
+ * 
+ * __dirname returns the values of directory where the current running file resides.
+ * 
+ */
+
+// main.js
+console.log(process.cwd()); // C:\Project
+console.log(__dirname); // C:\Project
+
+// script.js
+console.log(process.cwd()); // C:\Project
+console.log(__dirname); // C:\Project\lib
+
+
+
+
 
 
 
